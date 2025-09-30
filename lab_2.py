69a4182d2371231aaa4181fc8c439277bbebcf5b
Felicity H <felicityh15@gmail.com>
2025-09-29 18:22:34 -0700

    code yay

--- lab_2.py
+++ lab_2.py
@@ -89,20 +89,20 @@ class ForwardKinematics(Node):
 
         # T_1_2 (leg_front_l_1 to leg_front_l_2)
         ## TODO: Implement the transformation matrix from leg_front_l_1 to leg_front_l_2
-        T_1_2 = None
+        T_1_2 = translation(0, 0, -0.039) @ rotation_y(-1.57080) @ rotation_x(theta2)
 
         # T_2_3 (leg_front_l_2 to leg_front_l_3)
         ## TODO: Implement the transformation matrix from leg_front_l_2 to leg_front_l_3
-        T_2_3 = None
+        T_2_3 = translation(0, -0.0494, 0.0685) @ rotation_y(1.57080) @ rotation_z(-theta3)
 
         # T_3_ee (leg_front_l_3 to end-effector)
-        T_3_ee = None
+        T_3_ee = translation(0.06231, -0.06216, -0.018) 
 
         # TODO: Compute the final transformation. T_0_ee is the multiplication of the previous transformation matrices
-        T_0_ee = None
+        T_0_ee = T_0_1 @ T_1_2 @ T_2_3 @ T_3_ee
 
         # TODO: Extract the end-effector position. The end effector position is a 3x1 vector (not in homogenous coordinates)
-        end_effector_position = None
+        end_effector_position = (T_0_ee @ np.array([0, 0.088, 1]))[0:2]
 
         return end_effector_position
 
